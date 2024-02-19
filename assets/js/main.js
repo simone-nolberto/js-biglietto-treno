629a1781afb2c3cb2e61014730e19f4094d811fa
simone-nolberto <simone.nolberto@outlook.com>
2024-02-19 18:12:21 +0100

    update
    main structure working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,30 @@
-//let userMileage = Number(prompt ("Indica quanti chilometri vuoi percorrere"));
-//let userAge = Number(prompt ("Specifica la tua età"));
+let userMileage = Number(prompt ("Indica quanti chilometri vuoi percorrere"));
+let userAge = Number(prompt ("Specifica la tua età"));
 
 console.log(userMileage);
 console.log(userAge);
 
+/*
+(function formatAsPercentage(num) {
+    return new Intl.NumberFormat('default', {
+      style: 'percent',
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(num / 100);
+}
+ */
+
 const mileagePrice = 0.21;
-const underAgeDiscount = 0.20;
-const overDiscount = 0.40;
+const underAgeDiscount = 0.168;
+const overDiscount = 0.126;
+
+
+  
 
 if (userAge < 18){
-    console.log(userMileage * mileagePrice)
+    console.log(userMileage * underAgeDiscount)
 } else if (userAge >= 65){
-    console.log(userMileage * mileagePrice);
+    console.log(userMileage * overDiscount);
 } else {
     console.log(userMileage * mileagePrice);
 }
