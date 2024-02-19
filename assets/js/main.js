1853cd3abb7c3993bc89a3dc726b997c913a3c06
simone-nolberto <simone.nolberto@outlook.com>
2024-02-19 16:50:11 +0100

    add
    main input

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+//let userMileage = Number(prompt ("Indica quanti chilometri vuoi percorrere"));
+//let userAge = Number(prompt ("Specifica la tua età"));
+
+console.log(userMileage);
+console.log(userAge);
+
+const ticketPrice = 0.21;
+const underAgeDiscount = 0.20;
+const overDiscount = 0.40;
